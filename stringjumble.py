c3b26f21937ed315e0947174321bb726258df930
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-26 13:23:16 -0400

    Updated from Brython Server: 9/26/2016 1:23:15 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 
-letters = input("Please enter a string of text (the bigger the better): "
+letters = input("Please enter a string of text (the bigger the better): ")
 print("You entered "+letters+". Now jumble it: ")
-reverse = string[::-1]
+reverse = letters[::-1]
 print(reverse)
